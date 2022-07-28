16a4f7ef466f1c7b69064ec954f5d19971f5974c
Ibraheem Rodrigues <contact@ibraheemrodrigues.com>
2022-07-28 22:50:49 +0100

    Add init script & i3 config

---
+++ init.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+sudo apt install git curl i3 zsh feh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git config --global user.name "Ibraheem Rodrigues"
+git config --global user.email "contact@ibraheemrodrigues.com"
