f9bc699434032097171ff8e6b7c86f955b9a86f7
Ibraheem Rodrigues <28712361+IbraheemR@users.noreply.github.com>
2022-07-28 23:29:47 +0100

    use .gitconfig instead of setting from cli

--- init.sh
+++ init.sh
@@ -9,9 +9,6 @@ if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-git config --global user.name "Ibraheem Rodrigues"
-git config --global user.email "contact@ibraheemrodrigues.com"
-
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
 sudo apt autoremove -y
