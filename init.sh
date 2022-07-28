bef2d8138343bc1f8ad713c60a588dec27dff53a
Ibraheem Rodrigues <contact@ibraheemrodrigues.com>
2022-07-28 23:22:52 +0100

    Only install oh my zsh if not already installed

--- init.sh
+++ init.sh
@@ -2,7 +2,11 @@
 
 sudo apt install git curl i3 zsh feh
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# if ohmy zsh is not installed, install it
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
 
 git config --global user.name "Ibraheem Rodrigues"
 git config --global user.email "contact@ibraheemrodrigues.com"
