284243642bc7404b23e13ab220f5a9dbffae493f
Ibraheem Rodrigues <contact@ibraheemrodrigues.com>
2022-07-28 23:25:40 +0100

    Add -y

--- init.sh
+++ init.sh
@@ -2,17 +2,16 @@
 
 sudo apt update
 
-sudo apt install git curl i3 zsh feh
+sudo apt install -y git curl i3 zsh feh
 
 # if ohmy zsh is not installed, install it
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-
 git config --global user.name "Ibraheem Rodrigues"
 git config --global user.email "contact@ibraheemrodrigues.com"
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
-sudo apt autoremove
+sudo apt autoremove -y
