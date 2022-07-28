889daedcca1f3f65970d6cdb3beeab263a7d38f0
Ibraheem Rodrigues <contact@ibraheemrodrigues.com>
2022-07-28 23:24:30 +0100

    Add apt update

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+sudo apt update
+
 sudo apt install git curl i3 zsh feh
 
 # if ohmy zsh is not installed, install it
@@ -12,3 +14,5 @@ git config --global user.name "Ibraheem Rodrigues"
 git config --global user.email "contact@ibraheemrodrigues.com"
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
+sudo apt autoremove
